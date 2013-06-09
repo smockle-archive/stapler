edecfaebe0d4b0804fe8bade835002cf364d385c
Clay Miller <clay@smockle.com>
2013-06-09 03:31:42 -0500

    Add tests for String#is_int and String#is_range.

--- tests/string_test.rb
+++ tests/string_test.rb
@@ -3,11 +3,22 @@ require "test/unit"
  
 class TestString < Test::Unit::TestCase
   def test_is_int
-    assert true
+    assert !("four".is_int)
+    assert !("4.444".is_int)
+    assert !("4..4".is_int)
+    assert "4".is_int
   end
   
   def test_is_range
-    assert true
+    assert !("four".is_range)
+    assert !("4.444".is_range)
+    assert !("4".is_range)
+    assert !("4..".is_range)
+    assert !("4...".is_range)
+    assert !("4..8..15".is_range)
+    assert !("4...8...15".is_range)
+    assert "4..8".is_range
+    assert "4...8".is_range
   end
   
   def test_to_range
