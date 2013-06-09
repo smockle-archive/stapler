d3182b89b85bef1e71223f5a3166939b49ab0f37
Clay Miller <clay@smockle.com>
2013-06-09 17:12:50 -0500

    Update TestStapler#test_get

--- tests/stapler_test.rb
+++ tests/stapler_test.rb
@@ -8,6 +8,7 @@ class TestStapler < Test::Unit::TestCase
     @c = File.expand_path("../../tests/stapler_test_c.pdf", __FILE__)
     @input = File.expand_path("../../tests/stapler_test_input.pdf", __FILE__)
     @output = File.expand_path("../../tests/stapler_test_output.pdf", __FILE__)
+    @stapler = Stapler.new
     
     Prawn::Document.generate(@a) do |pdf|
       pdf.text("PAGE_A")
@@ -31,28 +32,27 @@ class TestStapler < Test::Unit::TestCase
     File.delete(@c)
     File.delete(@input)
     if File.exist?(@output) then File.delete(@output) end
+    @stapler = nil
   end
   
   def test_get
     # stapler get input.pdf
-    exception = assert_raise(ArgumentError) { Stapler.new.get(@input) }
+    exception = assert_raise(ArgumentError) { @stapler.get(@input) }
     assert_equal "Not enough arguments.", exception.message
       
     # stapler get input.pdf output.pdf
-    exception = assert_raise(ArgumentError) { Stapler.new.get(@input, @output) }
+    exception = assert_raise(ArgumentError) { @stapler.get(@input, @output) }
     assert_equal "Not enough arguments.", exception.message
       
-    # stapler get input.pdf 4..42
-    assert true
-    
-    # stapler get input.pdf 4
-    assert true
-    
     # stapler get input.pdf output.pdf 4..42
-    assert true
+    @stapler.get(@input, @output, "2..3")
+    assert File.exist?(@output)
+    assert_equal Prawn::Document.new(:template => @output).page_count, 2
     
     # stapler get input.pdf output.pdf 4
-    assert true
+    @stapler.get(@input, @output, "2")
+    assert File.exist?(@output)
+    assert_equal Prawn::Document.new(:template => @output).page_count, 1
   end
   
   def test_insert
