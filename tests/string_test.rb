37cff1942aa55d1bdc694edaada2d2f073211485
Clay Miller <clay@smockle.com>
2013-06-09 13:51:15 -0500

    Remove parentheses.

--- tests/string_test.rb
+++ tests/string_test.rb
@@ -23,19 +23,19 @@ class TestString < Test::Unit::TestCase
   
   def test_to_range
     exception = assert_raise(TypeError) { "four".to_range }
-    assert_equal("Cannot convert four to range.", exception.message)
+    assert_equal "Cannot convert four to range.", exception.message
     exception = assert_raise(TypeError) { "4.4".to_range }
-    assert_equal("Cannot convert 4.4 to range.", exception.message)
+    assert_equal "Cannot convert 4.4 to range.", exception.message
     exception = assert_raise(TypeError) { "4".to_range }
-    assert_equal("Cannot convert 4 to range.", exception.message)
+    assert_equal "Cannot convert 4 to range.", exception.message
     exception = assert_raise(TypeError) { "4..".to_range }
-    assert_equal("Cannot convert 4.. to range.", exception.message)
+    assert_equal "Cannot convert 4.. to range.", exception.message
     exception = assert_raise(TypeError) { "4...".to_range }
-    assert_equal("Cannot convert 4... to range.", exception.message)
+    assert_equal "Cannot convert 4... to range.", exception.message
     exception = assert_raise(TypeError) { "4..8..15".to_range }
-    assert_equal("Cannot convert 4..8..15 to range.", exception.message)
+    assert_equal "Cannot convert 4..8..15 to range.", exception.message
     exception = assert_raise(TypeError) { "4...8...15".to_range }
-    assert_equal("Cannot convert 4...8...15 to range.", exception.message)
+    assert_equal "Cannot convert 4...8...15 to range.", exception.message
     assert_equal "4..8".to_range, 4..8
     assert_equal "4...8".to_range, 4...8
   end
